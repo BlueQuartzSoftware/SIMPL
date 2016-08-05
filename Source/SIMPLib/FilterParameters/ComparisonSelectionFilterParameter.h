e52c5645fa9d9866b1ca88e9119b7621f05c2c1a
Joey Kleingers <joey.kleingers@bluequartz.net>
2016-08-05 10:58:38 -0400

    Replacing all ComparisonSelectionFilterParameter::New function uses with its new macro.

--- Source/SIMPLib/FilterParameters/ComparisonSelectionFilterParameter.h
+++ Source/SIMPLib/FilterParameters/ComparisonSelectionFilterParameter.h
@@ -41,6 +41,11 @@
 #include "SIMPLib/Common/ComparisonInputs.h"
 #include "SIMPLib/FilterParameters/FilterParameter.h"
 
+#define SIMPL_NEW_COMP_SEL_FP(...) \
+  _FP_GET_OVERRIDE(__VA_ARGS__, \
+  SIMPL_NEW_FP_9, SIMPL_NEW_FP_8, SIMPL_NEW_FP_7, SIMPL_NEW_FP_6, SIMPL_NEW_FP_5, SIMPL_NEW_FP_4)\
+  (ComparisonSelectionFilterParameter, __VA_ARGS__)
+
 class SIMPLib_EXPORT ComparisonSelectionFilterParameter : public FilterParameter
 {
   public:
