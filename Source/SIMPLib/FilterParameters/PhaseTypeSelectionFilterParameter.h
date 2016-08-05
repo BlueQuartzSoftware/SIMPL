be7187c960ef044b42a129681318012834e0659c
Joey Kleingers <joey.kleingers@bluequartz.net>
2016-08-05 17:39:12 -0400

    Replacing all PhaseTypeSelectionFilterParameter::New function uses with its new macro.

--- Source/SIMPLib/FilterParameters/PhaseTypeSelectionFilterParameter.h
+++ Source/SIMPLib/FilterParameters/PhaseTypeSelectionFilterParameter.h
@@ -41,6 +41,11 @@
 #include "SIMPLib/FilterParameters/FilterParameter.h"
 #include "SIMPLib/DataContainers/DataArrayPath.h"
 
+#define SIMPL_NEW_PHASETYPE_SELECTION_FP(...) \
+  _FP_GET_OVERRIDE(__VA_ARGS__, \
+  SIMPL_NEW_FP_9, SIMPL_NEW_FP_8, SIMPL_NEW_FP_7, SIMPL_NEW_FP_6, SIMPL_NEW_FP_5, SIMPL_NEW_FP_4)\
+  (PhaseTypeSelectionFilterParameter, __VA_ARGS__)
+
 class SIMPLib_EXPORT PhaseTypeSelectionFilterParameter : public FilterParameter
 {
 public:
