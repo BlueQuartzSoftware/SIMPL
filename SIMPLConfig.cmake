f6773f967a8df26f67cc5c00a4b4e4aac0a52f04
Joey Kleingers <joey.kleingers@bluequartz.net>
2017-10-26 15:42:40 -0400

    PKG: Installing SIMPLibTargets.cmake, and moving a few files to get the SIMPL installation to work with TomViz.

--- SIMPLConfig.cmake
+++ SIMPLConfig.cmake
@@ -1,5 +1,5 @@
-include("${CMAKE_CURRENT_LIST_DIR}/SIMPLibTargets.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/SIMPLibH5SupportTargets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/SIMPLibTargets.cmake")
 
 set(SIMPL_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../../include")
 
