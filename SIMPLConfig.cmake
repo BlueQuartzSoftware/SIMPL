108118e7c43e0b82ace994763696d3aa38d0697b
Joey Kleingers <joey.kleingers@bluequartz.net>
2017-10-31 12:00:14 -0400

    Changing include variable in config file so that projects linking against an install of SIMPL will configure correctly.

--- SIMPLConfig.cmake
+++ SIMPLConfig.cmake
@@ -1,4 +1,4 @@
-include("${CMAKE_CURRENT_LIST_DIR}/SIMPLibH5SupportTargets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/H5SupportTargets.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/SIMPLibTargets.cmake")
 
 set(SIMPL_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../../include")
