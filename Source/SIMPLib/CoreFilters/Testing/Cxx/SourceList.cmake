c931d18c0c0808f33354ff1d54baffc8db82b53c
Joey Kleingers <joey.kleingers@bluequartz.net>
2017-11-29 13:17:51 -0500

    Using ImportASCIIDataArray filter instead of ReadAngData filter in GenerateColorTableTest so that we only rely on filters that exist in SIMPL. Instead of copying the test files to the build folder, they are now just read from the source folder.

--- Source/SIMPLib/CoreFilters/Testing/Cxx/SourceList.cmake
+++ Source/SIMPLib/CoreFilters/Testing/Cxx/SourceList.cmake
@@ -1,4 +1,6 @@
 
+set(TEST_TESTFILES_DIR ${SIMPLib_SOURCE_DIR}/${SUBDIR_NAME}/Testing/Cxx/TestFiles)
+
 set(TEST_${SUBDIR_NAME}_NAMES
   ArrayCalculatorTest
   CombineAttributeArraysTest
@@ -38,16 +40,6 @@ SIMPL_ADD_UNIT_TEST_MOC_FILE(ExecuteProcessTest "${SIMPLib_SOURCE_DIR}/${SUBDIR_
 
 set(SIMPLTest_SOURCE_DIR ${SIMPLib_SOURCE_DIR}/Testing)
 set(SIMPLTest_BINARY_DIR ${SIMPLib_BINARY_DIR}/Testing)
-#-------------------------------------------------------------------------------
-#- This copies all the Prebuilt Pipeline files into the Build directory so the help
-#- works from the Build Tree
-add_custom_target(ImageASCIICopy ALL
-            COMMAND ${CMAKE_COMMAND} -E copy_directory ${SIMPLTest_SOURCE_DIR}/ImageASCII
-            ${SIMPLTest_BINARY_DIR}/ImageASCII/
-            COMMENT "Copying Test Images into Binary Directory")
-set_target_properties(ImageASCIICopy PROPERTIES FOLDER ZZ_COPY_FILES)
-
-
 
 # QT5_WRAP_CPP( execute_process_moc  ${SIMPLTest_SOURCE_DIR}/ExecuteProcessTest.cpp)
 # set_source_files_properties( ${execute_process_moc} PROPERTIES GENERATED TRUE)
