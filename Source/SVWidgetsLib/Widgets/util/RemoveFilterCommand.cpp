6b0ebe65321f190e22717d53a94e8bce9a918295
Joey Kleingers <joey.kleingers@gmail.com>
2018-05-16 14:57:21 -0400

    Clang formatting

--- Source/SVWidgetsLib/Widgets/util/RemoveFilterCommand.cpp
+++ Source/SVWidgetsLib/Widgets/util/RemoveFilterCommand.cpp
@@ -1,37 +1,37 @@
 /* ============================================================================
-* Copyright (c) 2009-2016 BlueQuartz Software, LLC
-*
-* Redistribution and use in source and binary forms, with or without modification,
-* are permitted provided that the following conditions are met:
-*
-* Redistributions of source code must retain the above copyright notice, this
-* list of conditions and the following disclaimer.
-*
-* Redistributions in binary form must reproduce the above copyright notice, this
-* list of conditions and the following disclaimer in the documentation and/or
-* other materials provided with the distribution.
-*
-* Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
-* contributors may be used to endorse or promote products derived from this software
-* without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-* The code contained herein was partially funded by the followig contracts:
-*    United States Air Force Prime Contract FA8650-07-D-5800
-*    United States Air Force Prime Contract FA8650-10-D-5210
-*    United States Prime Contract Navy N00173-07-C-2068
-*
-* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+ * Copyright (c) 2009-2016 BlueQuartz Software, LLC
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
+ * contributors may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The code contained herein was partially funded by the followig contracts:
+ *    United States Air Force Prime Contract FA8650-07-D-5800
+ *    United States Air Force Prime Contract FA8650-10-D-5210
+ *    United States Prime Contract Navy N00173-07-C-2068
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 #include "RemoveFilterCommand.h"
 
@@ -43,10 +43,10 @@
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
-RemoveFilterCommand::RemoveFilterCommand(AbstractFilter::Pointer filter, SVPipelineView *view, QString actionText, bool useAnimationOnFirstRun, QUndoCommand* parent)
-  : QUndoCommand(parent)
-  , m_PipelineView(view)
-  , m_UseAnimationOnFirstRun(useAnimationOnFirstRun)
+RemoveFilterCommand::RemoveFilterCommand(AbstractFilter::Pointer filter, SVPipelineView* view, QString actionText, bool useAnimationOnFirstRun, QUndoCommand* parent)
+: QUndoCommand(parent)
+, m_PipelineView(view)
+, m_UseAnimationOnFirstRun(useAnimationOnFirstRun)
 {
   if(nullptr == filter || nullptr == view)
   {
@@ -66,11 +66,11 @@ RemoveFilterCommand::RemoveFilterCommand(AbstractFilter::Pointer filter, SVPipel
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
-RemoveFilterCommand::RemoveFilterCommand(std::vector<AbstractFilter::Pointer> filters, SVPipelineView *view, QString actionText, bool useAnimationOnFirstRun, QUndoCommand* parent)
-  : QUndoCommand(parent)
-  , m_PipelineView(view)
-  , m_Filters(filters)
-  , m_UseAnimationOnFirstRun(useAnimationOnFirstRun)
+RemoveFilterCommand::RemoveFilterCommand(std::vector<AbstractFilter::Pointer> filters, SVPipelineView* view, QString actionText, bool useAnimationOnFirstRun, QUndoCommand* parent)
+: QUndoCommand(parent)
+, m_PipelineView(view)
+, m_Filters(filters)
+, m_UseAnimationOnFirstRun(useAnimationOnFirstRun)
 {
   if(nullptr == view)
   {
@@ -81,7 +81,7 @@ RemoveFilterCommand::RemoveFilterCommand(std::vector<AbstractFilter::Pointer> fi
 
   PipelineModel* model = m_PipelineView->getPipelineModel();
 
-  for (size_t i = 0; i < m_Filters.size(); i++)
+  for(size_t i = 0; i < m_Filters.size(); i++)
   {
     QModelIndex index = model->indexOfFilter(m_Filters[i].get());
     m_FilterRows.push_back(index.row());
@@ -115,10 +115,10 @@ void RemoveFilterCommand::undo()
   emit m_PipelineView->pipelineChanged();
 
   QString statusMessage;
-  if (m_Filters.size() > 1)
+  if(m_Filters.size() > 1)
   {
     QString indexesString = QObject::tr("%1").arg(m_FilterRows[0] + 1);
-    for (size_t i = 1; i < m_FilterRows.size(); i++)
+    for(size_t i = 1; i < m_FilterRows.size(); i++)
     {
       indexesString.append(", ");
       indexesString.append(QObject::tr("%1").arg(m_FilterRows[i] + 1));
@@ -137,11 +137,10 @@ void RemoveFilterCommand::undo()
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
-bool variantCompare(const QVariant &v1, const QVariant &v2)
+bool variantCompare(const QVariant& v1, const QVariant& v2)
 {
   return v1.toInt() > v2.toInt();
 }
-
 
 // -----------------------------------------------------------------------------
 //
@@ -154,10 +153,10 @@ void RemoveFilterCommand::redo()
   }
 
   QString statusMessage;
-  if (m_Filters.size() > 1)
+  if(m_Filters.size() > 1)
   {
     QString indexesString = QObject::tr("%1").arg(m_FilterRows[0] + 1);
-    for (size_t i = 1; i < m_FilterRows.size(); i++)
+    for(size_t i = 1; i < m_FilterRows.size(); i++)
     {
       indexesString.append(", ");
       indexesString.append(QObject::tr("%1").arg(m_FilterRows[i] + 1));
@@ -169,7 +168,7 @@ void RemoveFilterCommand::redo()
     statusMessage = QObject::tr("Removed '%1' filter at index %2").arg(m_Filters[0]->getHumanLabel()).arg(m_FilterRows[0] + 1);
   }
 
-  if (m_FirstRun == false)
+  if(m_FirstRun == false)
   {
     statusMessage.prepend("Redo \"");
     statusMessage.append('\"');
@@ -201,7 +200,7 @@ void RemoveFilterCommand::addFilter(AbstractFilter::Pointer filter, int insertio
 
   connectFilterSignalsSlots(filter);
 
-  if (filter->getEnabled() == false)
+  if(filter->getEnabled() == false)
   {
     model->setData(filterIndex, static_cast<int>(PipelineItem::WidgetState::Disabled), PipelineModel::WidgetStateRole);
   }
@@ -211,9 +210,8 @@ void RemoveFilterCommand::addFilter(AbstractFilter::Pointer filter, int insertio
   PipelineItemSlideAnimation* animation = new PipelineItemSlideAnimation(model, QPersistentModelIndex(filterIndex), filterRect.width(), PipelineItemSlideAnimation::AnimationDirection::EnterRight);
   model->setData(QPersistentModelIndex(filterIndex), PipelineItem::AnimationType::Add, PipelineModel::Roles::AnimationTypeRole);
 
-  QObject::connect(animation, &PipelineItemSlideAnimation::finished, [=] {
-    model->setData(QPersistentModelIndex(filterIndex), PipelineItem::AnimationType::None, PipelineModel::Roles::AnimationTypeRole);
-  });
+  QObject::connect(animation, &PipelineItemSlideAnimation::finished,
+                   [=] { model->setData(QPersistentModelIndex(filterIndex), PipelineItem::AnimationType::None, PipelineModel::Roles::AnimationTypeRole); });
   animation->start(QAbstractAnimation::DeleteWhenStopped);
 }
 
@@ -232,7 +230,7 @@ void RemoveFilterCommand::removeFilter(AbstractFilter::Pointer filter)
 
   QRect filterRect = m_PipelineView->visualRect(index);
 
-  if (m_UseAnimationOnFirstRun == false && m_FirstRun == true)
+  if(m_UseAnimationOnFirstRun == false && m_FirstRun == true)
   {
     model->removeRow(persistentIndex.row());
   }
@@ -241,9 +239,7 @@ void RemoveFilterCommand::removeFilter(AbstractFilter::Pointer filter)
     PipelineItemSlideAnimation* animation = new PipelineItemSlideAnimation(model, persistentIndex, filterRect.width(), PipelineItemSlideAnimation::AnimationDirection::ExitRight);
     model->setData(persistentIndex, PipelineItem::AnimationType::Remove, PipelineModel::Roles::AnimationTypeRole);
 
-    QObject::connect(animation, &PipelineItemSlideAnimation::finished, [=] {
-      model->removeRow(persistentIndex.row());
-    });
+    QObject::connect(animation, &PipelineItemSlideAnimation::finished, [=] { model->removeRow(persistentIndex.row()); });
     animation->start(QAbstractAnimation::DeleteWhenStopped);
   }
 }
