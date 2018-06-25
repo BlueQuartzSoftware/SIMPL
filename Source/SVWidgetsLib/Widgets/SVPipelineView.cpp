8f430526fcf3070e4859fb2a3925d0b6e55f48a8
Michael Jackson <mike.jackson@bluequartz.net>
2018-06-25 16:14:52 -0400

    Guard access to raw pointer.

--- Source/SVWidgetsLib/Widgets/SVPipelineView.cpp
+++ Source/SVWidgetsLib/Widgets/SVPipelineView.cpp
@@ -1260,7 +1260,10 @@ void SVPipelineView::dropEvent(QDropEvent* event)
       // Set the text of the drag command
       QString text = cmd->text();
 
-      m_MoveCommand->setText(text);
+      if(m_MoveCommand)
+      {
+        m_MoveCommand->setText(text);
+      }
 
       // The overall drag command already has a child command that removed the filters initially, and
       // has already been placed on the undo stack and executed.  This new child command needs to be executed
