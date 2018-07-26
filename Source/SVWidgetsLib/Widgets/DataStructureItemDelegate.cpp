8a32eb0283287266b152addaa14f0947ada084d8
Michael Jackson <mike.jackson@bluequartz.net>
2018-07-26 10:44:05 -0400

    Reverse a commit that landed with bad color information.

--- Source/SVWidgetsLib/Widgets/DataStructureItemDelegate.cpp
+++ Source/SVWidgetsLib/Widgets/DataStructureItemDelegate.cpp
@@ -405,8 +405,7 @@ void DataStructureItemDelegate::paint(QPainter* painter, const QStyleOptionViewI
 
   // Draw Text
   QRect textRect(op.rect.x() + textOffset, op.rect.y() + fm.descent() / 2, op.rect.width(), op.rect.height());
- // painter->setPen(op.palette.color(QPalette::Normal, QPalette::WindowText));
-  painter->setPen(styles->getQTreeViewItem_color());
+  painter->setPen(op.palette.color(QPalette::Normal, QPalette::WindowText));
   painter->drawText(textRect, text);
 
   // Draw icon for created paths
