8163f8b6a411b50311e3a9ce0c25963091259722
Jared Duffey <jared.duffey@bluequartz.net>
2018-09-24 10:00:21 -0400

    Changed loop condition to use getSize()

--- Source/SIMPLib/CoreFilters/Testing/Cxx/FeatureDataCSVWriterTest.cpp
+++ Source/SIMPLib/CoreFilters/Testing/Cxx/FeatureDataCSVWriterTest.cpp
@@ -1,6 +1,38 @@
-// -----------------------------------------------------------------------------
-// Insert your license & copyright information here
-// -----------------------------------------------------------------------------
+/* ============================================================================
+ * Copyright (c) 2009-2016 BlueQuartz Software, LLC
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
+ * contributors may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The code contained herein was partially funded by the followig contracts:
+ *    United States Air Force Prime Contract FA8650-07-D-5800
+ *    United States Air Force Prime Contract FA8650-10-D-5210
+ *    United States Prime Contract Navy N00173-07-C-2068
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 #pragma once
 
 #include <QtCore/QCoreApplication>
@@ -375,7 +407,7 @@ public:
     // Create DataArrayInt (add in under checkFilter)
 
     DataArray<int32_t>::Pointer da = DataArray<int32_t>::CreateArray(tupleDims, cDims, k_DataArrayIntName);
-    for(size_t i = 0; i < (da->getNumberOfTuples() * da->getNumberOfComponents()); i++)
+    for(size_t i = 0; i < da->getSize(); i++)
     {
       da->setValue(i, i);
     }
@@ -384,7 +416,7 @@ public:
     // Create DataArrayFloat
 
     DataArray<float>::Pointer daFloat = DataArray<float>::CreateArray(tupleDims, cDimsFloat, k_DataArrayFloatName);
-    for(size_t i = 0; i < (daFloat->getNumberOfTuples() * daFloat->getNumberOfComponents()); i++)
+    for(size_t i = 0; i < daFloat->getSize(); i++)
     {
       if(i % 2 == 0)
       {
@@ -408,7 +440,7 @@ public:
     am->addAttributeArray(k_NeighborListName, nl);
 
     NeighborList<int32_t>::Pointer nl2 = NeighborList<int32_t>::CreateArray(tupleDims, cDimsNeighbor, k_NeighborListName2);
-    for (size_t i = 0; i < nl2->getNumberOfTuples(); i++)
+    for(size_t i = 0; i < nl2->getNumberOfTuples(); i++)
     {
       nl2->addEntry(i, i * 3);
       nl2->addEntry(i, i);
