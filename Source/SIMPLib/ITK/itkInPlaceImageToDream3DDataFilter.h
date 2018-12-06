3cb325c3fb0adc01bf173693a2f09f159c8eabb4
Matt McCormick <matt@mmmccormick.com>
2018-12-06 14:55:52 -0500

    Address VerifyPreconditions const change in ITK 5 master (#272)
    This requires ITK 5 Beta 2 or ITK 4.13.2, both are currently to-be-released.

--- Source/SIMPLib/ITK/itkInPlaceImageToDream3DDataFilter.h
+++ Source/SIMPLib/ITK/itkInPlaceImageToDream3DDataFilter.h
@@ -36,6 +36,7 @@ public:
   virtual void SetInput(const ImageType* image);
 
   DecoratorType* GetOutput();
+  const DecoratorType* GetOutput() const;
 
   void SetDataContainer(DataContainer::Pointer dc);
 
@@ -54,13 +55,13 @@ protected:
   InPlaceImageToDream3DDataFilter();
   virtual ~InPlaceImageToDream3DDataFilter();
 
-  virtual void VerifyPreconditions() override;
+  virtual void VerifyPreconditions() ITKv5_CONST override;
   ProcessObject::DataObjectPointer MakeOutput(ProcessObject::DataObjectPointerArraySizeType) override;
 
   virtual void GenerateData() override;
   virtual void GenerateOutputInformation() override;
 
-  void CheckValidArrayPathComponentName(std::string var);
+  void CheckValidArrayPathComponentName(std::string var) const;
 
 private:
   using Superclass::SetInput;
