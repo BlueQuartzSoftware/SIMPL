9a59c3f3a77fb52eba283b365b0b9a5cac9ca30a
Tom Platt <tom.platt@bluequartz.net>
2019-01-02 11:59:59 -0500

    Adding MultiDataContainerSelectionFilterParameter and associated widget for selecting a list of data containers

--- Source/SVWidgetsLib/FilterParameterWidgets/SourceList.cmake
+++ Source/SVWidgetsLib/FilterParameterWidgets/SourceList.cmake
@@ -33,6 +33,7 @@ set(SIMPLView_PARAMETER_WIDGETS
     IntWidget
     LinkedBooleanWidget
     MultiAttributeMatrixSelectionWidget
+    MultiDataContainerSelectionWidget
     MultiDataArraySelectionWidget
     NumericTypeWidget
     ParagraphWidget
