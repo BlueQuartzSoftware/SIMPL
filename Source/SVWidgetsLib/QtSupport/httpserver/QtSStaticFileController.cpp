c67eb4400b7a72a01dc8902c795c7931b67f6308
Michael Jackson <mike.jackson@bluequartz.net>
2019-01-10 15:39:41 -0500

    Fixing anther MSVC compile error
    Was not caught on other platforms due to use of platform specific
    preprocessor directives
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Source/SVWidgetsLib/QtSupport/httpserver/QtSStaticFileController.cpp
+++ Source/SVWidgetsLib/QtSupport/httpserver/QtSStaticFileController.cpp
@@ -22,11 +22,7 @@ QtSStaticFileController::QtSStaticFileController(ServerSettings* settings, QObje
   if(!(docroot.startsWith(":/") || docroot.startsWith("qrc://")))
   {
 // Convert relative path to absolute, based on the directory of the config file.
-#ifdef Q_OS_WIN32
-    if(QDir::isRelativePath(docroot) && settings->format() != ServerSettings::NativeFormat)
-#else
     if(QDir::isRelativePath(docroot))
-#endif
     {
       QFileInfo configFile(settings->configFileName);
       docroot = QFileInfo(configFile.absolutePath(), docroot).absoluteFilePath();
