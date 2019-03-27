28aaceab36a19a2e61f24bdb2b5faebebebe9a21
Matthew Marine <matthew.marine@bluequartz.net>
2019-03-27 09:22:16 -0400

    Ran clang-format on ColorTable.cpp for readability

--- Source/SIMPLib/Utilities/ColorTable.cpp
+++ Source/SIMPLib/Utilities/ColorTable.cpp
@@ -1,37 +1,37 @@
 /* ============================================================================
-* Copyright (c) 2009-2016 BlueQuartz Software, LLC
-*
-* Redistribution and use in source and binary forms, with or without modification,
-* are permitted provided that the following conditions are met:
-*
-* Redistributions of source code must retain the above copyright notice, this
-* list of conditions and the following disclaimer.
-*
-* Redistributions in binary form must reproduce the above copyright notice, this
-* list of conditions and the following disclaimer in the documentation and/or
-* other materials provided with the distribution.
-*
-* Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
-* contributors may be used to endorse or promote products derived from this software
-* without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-* The code contained herein was partially funded by the followig contracts:
-*    United States Air Force Prime Contract FA8650-07-D-5800
-*    United States Air Force Prime Contract FA8650-10-D-5210
-*    United States Prime Contract Navy N00173-07-C-2068
-*
-* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+ * Copyright (c) 2009-2019 BlueQuartz Software, LLC
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
+ * contributors may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The code contained herein was partially funded by the followig contracts:
+ *    United States Air Force Prime Contract FA8650-07-D-5800
+ *    United States Air Force Prime Contract FA8650-10-D-5210
+ *    United States Prime Contract Navy N00173-07-C-2068
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 #include "ColorTable.h"
 
@@ -77,7 +77,7 @@ void SIMPLColorTable::GetColorTable(int numColors, QVector<float>& colorsOut)
     float pos = i * stepSize; // [0, 1] range
     int currColorBin = static_cast<int>(pos / nodeStepSize);
     float currFraction = (pos / nodeStepSize) - currColorBin;
-    
+
     float r;
     float g;
     float b;
@@ -108,15 +108,15 @@ std::vector<unsigned char> SIMPLColorTable::GetColorTable(size_t numColors, QJso
 {
   const size_t controlColorsCount = colorControlPoints.count() / 4;
   const size_t numComponents = 4;
-  std::vector<std::vector<double> > controlPoints(controlColorsCount, std::vector<double>(numComponents));
+  std::vector<std::vector<double>> controlPoints(controlColorsCount, std::vector<double>(numComponents));
 
   // Migrate colorControlPoints values from QJsonArray to 2D array.  Store A-values in binPoints vector.
   std::vector<float> binPoints;
-  for(size_t i = 0; i<controlColorsCount; i++)
+  for(size_t i = 0; i < controlColorsCount; i++)
   {
-    for(size_t j = 0; j<numComponents; j++)
+    for(size_t j = 0; j < numComponents; j++)
     {
-      controlPoints[i][j] = static_cast<float>(colorControlPoints[numComponents*i + j].toDouble());
+      controlPoints[i][j] = static_cast<float>(colorControlPoints[numComponents * i + j].toDouble());
       if(j == 0)
       {
         binPoints.push_back(controlPoints[i][j]);
@@ -127,7 +127,7 @@ std::vector<unsigned char> SIMPLColorTable::GetColorTable(size_t numColors, QJso
   // Normalize binPoints values
   const float min = binPoints[0];
   const float max = binPoints[binPoints.size() - 1];
-  for(size_t i=0; i<binPoints.size(); i++)
+  for(size_t i = 0; i < binPoints.size(); i++)
   {
     binPoints[i] = (binPoints[i] - min) / (max - min);
   }
@@ -141,7 +141,7 @@ std::vector<unsigned char> SIMPLColorTable::GetColorTable(size_t numColors, QJso
     const float allColorVal = static_cast<float>(i) * colorStepSize;
 
     // If we have crossed into the next color bin, increment the currentBinIndex variable.
-    if(currentBinIndex+1 < binPoints.size() && allColorVal > binPoints[currentBinIndex+1])
+    if(currentBinIndex + 1 < binPoints.size() && allColorVal > binPoints[currentBinIndex + 1])
     {
       // We have crossed into the next bin
       currentBinIndex++;
@@ -151,7 +151,7 @@ std::vector<unsigned char> SIMPLColorTable::GetColorTable(size_t numColors, QJso
     float currFraction = 0.0f;
     if(currentBinIndex + 1 < binPoints.size())
     {
-      currFraction = (allColorVal - binPoints[currentBinIndex]) / (binPoints[currentBinIndex+1] - binPoints[currentBinIndex]);
+      currFraction = (allColorVal - binPoints[currentBinIndex]) / (binPoints[currentBinIndex + 1] - binPoints[currentBinIndex]);
     }
     else
     {
@@ -187,7 +187,3 @@ std::vector<unsigned char> SIMPLColorTable::GetColorTable(size_t numColors, QJso
 
   return generatedColors;
 }
-
-
-
-
