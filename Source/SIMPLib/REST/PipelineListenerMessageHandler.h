edfd8c3c1d60487fbc29b95eed4cc19d89f53d78
Joey Kleingers <joey.kleingers@bluequartz.net>
2019-04-05 12:56:59 -0400

    Clang format PipelineListenerMessageHandler.h

--- Source/SIMPLib/REST/PipelineListenerMessageHandler.h
+++ Source/SIMPLib/REST/PipelineListenerMessageHandler.h
@@ -48,57 +48,57 @@ class PipelineListener;
  */
 class SIMPLib_EXPORT PipelineListenerMessageHandler : public AbstractMessageHandler
 {
-  public:
-    PipelineListenerMessageHandler(PipelineListener* listener);
+public:
+  PipelineListenerMessageHandler(PipelineListener* listener);
 
-    /**
-     * @brief Streams incoming PipelineErrorMessages to the error log and stores the message
-     * for later use.
-     */
-    virtual void processMessage(const PipelineErrorMessage* msg) const override;
+  /**
+   * @brief Streams incoming PipelineErrorMessages to the error log and stores the message
+   * for later use.
+   */
+  virtual void processMessage(const PipelineErrorMessage* msg) const override;
 
-    /**
-     * @brief Stores the incoming PipelineProgressMessages for later use.
-     */
-    virtual void processMessage(const PipelineProgressMessage* msg) const override;
+  /**
+   * @brief Stores the incoming PipelineProgressMessages for later use.
+   */
+  virtual void processMessage(const PipelineProgressMessage* msg) const override;
 
-    /**
-     * @brief Streams incoming PipelineStatusMessages to the status log and stores the message
-     * for later use.
-     */
-    virtual void processMessage(const PipelineStatusMessage* msg) const override;
+  /**
+   * @brief Streams incoming PipelineStatusMessages to the status log and stores the message
+   * for later use.
+   */
+  virtual void processMessage(const PipelineStatusMessage* msg) const override;
 
-    /**
-     * @brief Streams incoming PipelineWarningMessages to the warning log and stores the message
-     * for later use.
-     */
-    virtual void processMessage(const PipelineWarningMessage* msg) const override;
+  /**
+   * @brief Streams incoming PipelineWarningMessages to the warning log and stores the message
+   * for later use.
+   */
+  virtual void processMessage(const PipelineWarningMessage* msg) const override;
 
-    /**
-     * @brief Streams incoming FilterErrorMessages to the error log and stores the message
-     * for later use.
-     */
-    virtual void processMessage(const FilterErrorMessage* msg) const override;
+  /**
+   * @brief Streams incoming FilterErrorMessages to the error log and stores the message
+   * for later use.
+   */
+  virtual void processMessage(const FilterErrorMessage* msg) const override;
 
-    /**
-     * @brief Stores the incoming FilterProgressMessages for later use.
-     */
-    virtual void processMessage(const FilterProgressMessage* msg) const override;
+  /**
+   * @brief Stores the incoming FilterProgressMessages for later use.
+   */
+  virtual void processMessage(const FilterProgressMessage* msg) const override;
 
-    /**
-     * @brief Streams incoming FilterStatusMessages to the status log and stores the message
-     * for later use.
-     */
-    virtual void processMessage(const FilterStatusMessage* msg) const override;
+  /**
+   * @brief Streams incoming FilterStatusMessages to the status log and stores the message
+   * for later use.
+   */
+  virtual void processMessage(const FilterStatusMessage* msg) const override;
 
-    /**
-     * @brief Streams incoming FilterWarningMessages to the warning log and stores the message
-     * for later use.
-     */
-    virtual void processMessage(const FilterWarningMessage* msg) const override;
+  /**
+   * @brief Streams incoming FilterWarningMessages to the warning log and stores the message
+   * for later use.
+   */
+  virtual void processMessage(const FilterWarningMessage* msg) const override;
 
-  private:
-    PipelineListener* m_Listener = nullptr;
+private:
+  PipelineListener* m_Listener = nullptr;
 
-    void streamToLog(const QString &msgString, QFile* log) const;
+  void streamToLog(const QString& msgString, QFile* log) const;
 };
