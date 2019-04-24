98232abd26093e69b3b1b9a2be91b98ff385f07e
Joey Kleingers <joey.kleingers@bluequartz.net>
2019-04-24 07:35:58 -0400

    Revert "Topic/2 d image import fix (#342)" (#344)
    This reverts commit fa06dd2b07f075f7ed8e7191ee1e06594c0410a6.

--- Source/SIMPLib/ITK/itkInPlaceImageToDream3DDataFilter.hxx
+++ Source/SIMPLib/ITK/itkInPlaceImageToDream3DDataFilter.hxx
@@ -1,21 +1,23 @@
 #pragma once
 
-#include "SIMPLib/Geometry/ImageGeom.h"
-#include "itkGetComponentsDimensions.h"
 #include "itkInPlaceImageToDream3DDataFilter.h"
+#include "itkGetComponentsDimensions.h"
+#include "SIMPLib/Geometry/ImageGeom.h"
 #include <QString>
 
 namespace itk
 {
 
-template <typename PixelType, unsigned int VDimension>
-InPlaceImageToDream3DDataFilter<PixelType, VDimension>::InPlaceImageToDream3DDataFilter()
+template<typename PixelType, unsigned int VDimension>
+InPlaceImageToDream3DDataFilter<PixelType,VDimension>
+::InPlaceImageToDream3DDataFilter()
 {
   m_DataArrayName = (SIMPL::Defaults::CellAttributeMatrixName).toStdString();
   m_AttributeMatrixArrayName = (SIMPL::CellData::ImageData).toStdString();
   // Create the output. We use static_cast<> here because we know the default
   // output must be of type DecoratorType
-  typename DecoratorType::Pointer output = static_cast<DecoratorType*>(this->MakeOutput(0).GetPointer());
+  typename DecoratorType::Pointer output =
+    static_cast< DecoratorType * >(this->MakeOutput(0).GetPointer());
   this->ProcessObject::SetNumberOfRequiredOutputs(1);
   this->ProcessObject::SetNthOutput(0, output.GetPointer());
   this->SetNumberOfRequiredInputs(1);
@@ -23,40 +25,55 @@ InPlaceImageToDream3DDataFilter<PixelType, VDimension>::InPlaceImageToDream3DDat
   m_InPlace = true;
 }
 
-template <typename PixelType, unsigned int VDimension>
-void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::SetDataContainer(DataContainer::Pointer dc)
+
+template< typename PixelType, unsigned int VDimension>
+void
+InPlaceImageToDream3DDataFilter< PixelType, VDimension >
+::SetDataContainer(DataContainer::Pointer dc)
 {
-  DecoratorType* outputPtr = this->GetOutput();
+  DecoratorType *outputPtr = this->GetOutput();
   outputPtr->Set(dc);
 }
 
-template <typename PixelType, unsigned int VDimension>
-ProcessObject::DataObjectPointer InPlaceImageToDream3DDataFilter<PixelType, VDimension>::MakeOutput(ProcessObject::DataObjectPointerArraySizeType)
+
+
+template< typename PixelType, unsigned int VDimension>
+ProcessObject::DataObjectPointer
+InPlaceImageToDream3DDataFilter< PixelType, VDimension >
+::MakeOutput(ProcessObject::DataObjectPointerArraySizeType)
 {
   return DecoratorType::New().GetPointer();
 }
 
-template <typename PixelType, unsigned int VDimension>
-InPlaceImageToDream3DDataFilter<PixelType, VDimension>::~InPlaceImageToDream3DDataFilter()
+
+template<typename PixelType, unsigned int VDimension>
+InPlaceImageToDream3DDataFilter<PixelType, VDimension>
+::~InPlaceImageToDream3DDataFilter()
 {
 }
 
-template <typename PixelType, unsigned int VDimension>
-void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::SetInput(const ImageType* input)
+template< typename PixelType, unsigned int VDimension>
+void
+InPlaceImageToDream3DDataFilter< PixelType, VDimension >
+::SetInput(const ImageType *input)
 {
   // Process object is not const-correct so the const_cast is required here
-  this->ProcessObject::SetNthInput(0, const_cast<ImageType*>(input));
+  this->ProcessObject::SetNthInput(0,
+    const_cast< ImageType * >(input));
 }
 
-template <typename PixelType, unsigned int VDimension>
-void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::GenerateOutputInformation()
+
+template<typename PixelType, unsigned int VDimension>
+void
+InPlaceImageToDream3DDataFilter<PixelType, VDimension>
+::GenerateOutputInformation()
 {
-  DecoratorType* outputPtr = this->GetOutput();
+  DecoratorType *outputPtr = this->GetOutput();
   DataContainer::Pointer dataContainer = outputPtr->Get();
-  // float tol = 0.000001;
+  //float tol = 0.000001;
   QVector<float> torigin(3, 0);
   QVector<float> tspacing(3, 1);
-  QVector<size_t> tDims(3, 0);
+  QVector<size_t> tDims(3, 1);
   // Get Input image properties
   ImagePointer inputPtr = dynamic_cast<ImageType*>(this->GetInput(0));
   typename ImageType::PointType origin = inputPtr->GetOrigin();
@@ -65,10 +82,10 @@ void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::GenerateOutputInfor
   //// Create image geometry (data container)
   ImageGeom::Pointer imageGeom;
   IGeometry::Pointer geom = dataContainer->getGeometry();
-  if(geom)
+  if (geom)
   {
     imageGeom = std::dynamic_pointer_cast<ImageGeom>(geom);
-    if(!imageGeom)
+    if (!imageGeom)
     {
       itkExceptionMacro("Data container contains a geometry that is not ImageGeometry");
     }
@@ -76,16 +93,16 @@ void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::GenerateOutputInfor
   else
   {
     imageGeom = ImageGeom::CreateGeometry(SIMPL::Geometry::ImageGeometry);
-    if(!imageGeom)
+    if (!imageGeom)
     {
       itkExceptionMacro("Could not create image geometry");
     }
   }
-  for(size_t i = 0; i < VDimension; i++)
+  for (size_t i = 0; i < VDimension; i++)
   {
-    torigin[i] = origin[i];
-    tspacing[i] = spacing[i];
-    tDims[i] = size[i];
+	  torigin[i] = origin[i];
+	  tspacing[i] = spacing[i];
+	  tDims[i] = size[i];
   }
   imageGeom->setOrigin(FloatVec3Type(torigin[0], torigin[1], torigin[2]));
   imageGeom->setSpacing(FloatVec3Type(tspacing[0], tspacing[1], tspacing[2]));
@@ -93,10 +110,12 @@ void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::GenerateOutputInfor
   dataContainer->setGeometry(imageGeom);
 }
 
-template <typename PixelType, unsigned int VDimension>
-void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::GenerateData()
+template<typename PixelType, unsigned int VDimension>
+void
+InPlaceImageToDream3DDataFilter<PixelType, VDimension>
+::GenerateData()
 {
-  DecoratorType* outputPtr = this->GetOutput();
+  DecoratorType *outputPtr = this->GetOutput();
   DataContainer::Pointer dataContainer = outputPtr->Get();
   ImagePointer inputPtr = dynamic_cast<ImageType*>(this->GetInput(0));
   // Create data array
@@ -106,54 +125,56 @@ void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::GenerateData()
   QVector<size_t> attrMatDims = imageGeom->getDimensions().toContainer<QVector<size_t>>();
 
   AttributeMatrix::Pointer attrMat;
-  if(dataContainer->doesAttributeMatrixExist(m_AttributeMatrixArrayName.c_str()))
+  if( dataContainer->doesAttributeMatrixExist(m_AttributeMatrixArrayName.c_str()))
   {
-    attrMat = dataContainer->getAttributeMatrix(m_AttributeMatrixArrayName.c_str());
-    // Check that attribute matrix type is 'cell'
-    if(attrMat->getType() != AttributeMatrix::Type::Cell)
-    {
-      itkExceptionMacro("Attribute matrix is not of type AttributeMatrix::Type::Cell.");
-    }
-    // Check that if size does not match, there are no other data array than the one we expect.
-    // That makes it possible to modify the attribute matrix without having to worry.
-    QVector<size_t> matDims = attrMat->getTupleDimensions();
-    if(matDims != tDims)
-    {
-      if(!((attrMat->doesAttributeArrayExist(m_DataArrayName.c_str()) && attrMat->getNumAttributeArrays() == 1) ||
-           !(attrMat->doesAttributeArrayExist(m_DataArrayName.c_str()) && attrMat->getNumAttributeArrays() == 0)))
-      {
-        itkExceptionMacro("Tuples dimension of existing matrix array do not match image size and other attribute arrays are contained in this attribute matrix.");
-      }
-      dataContainer->removeAttributeMatrix(m_AttributeMatrixArrayName.c_str());
-      attrMat = dataContainer->createAndAddAttributeMatrix(attrMatDims, m_AttributeMatrixArrayName.c_str(), AttributeMatrix::Type::Cell);
-    }
+	attrMat = dataContainer->getAttributeMatrix(m_AttributeMatrixArrayName.c_str());
+	// Check that attribute matrix type is 'cell'
+	if (attrMat->getType() != AttributeMatrix::Type::Cell)
+	{
+		itkExceptionMacro("Attribute matrix is not of type AttributeMatrix::Type::Cell.");
+	}
+	// Check that if size does not match, there are no other data array than the one we expect.
+	// That makes it possible to modify the attribute matrix without having to worry.
+	QVector<size_t> matDims = attrMat->getTupleDimensions();
+	if (matDims != tDims)
+	{
+    if (! ((attrMat->doesAttributeArrayExist(m_DataArrayName.c_str()) && attrMat->getNumAttributeArrays() == 1)
+      || ! (attrMat->doesAttributeArrayExist(m_DataArrayName.c_str()) && attrMat->getNumAttributeArrays() == 0)) )
+		{
+			itkExceptionMacro("Tuples dimension of existing matrix array do not match image size and other attribute arrays are contained in this attribute matrix.");
+		}
+		dataContainer->removeAttributeMatrix(m_AttributeMatrixArrayName.c_str());
+		attrMat = dataContainer->createAndAddAttributeMatrix(tDims, m_AttributeMatrixArrayName.c_str(), AttributeMatrix::Type::Cell);
+	}
   }
   else
   {
-    attrMat = dataContainer->createAndAddAttributeMatrix(attrMatDims, m_AttributeMatrixArrayName.c_str(), AttributeMatrix::Type::Cell);
+    attrMat = dataContainer->createAndAddAttributeMatrix(tDims, m_AttributeMatrixArrayName.c_str(), AttributeMatrix::Type::Cell);
   }
   // Checks if doesAttributeArray exists and remove it if it is the case
-  if(attrMat->doesAttributeArrayExist(m_DataArrayName.c_str()))
+  if (attrMat->doesAttributeArrayExist(m_DataArrayName.c_str()))
   {
-    IDataArray::Pointer attrArray = attrMat->getAttributeArray(m_DataArrayName.c_str());
-    if(attrArray->getNumberOfTuples() != attrMat->getNumberOfTuples())
-    {
-      attrMat->removeAttributeArray(m_DataArrayName.c_str());
-    }
+	  IDataArray::Pointer attrArray = attrMat->getAttributeArray(m_DataArrayName.c_str());
+	  if (attrArray->getNumberOfTuples() != imageGeom->getNumberOfElements())
+	  {
+		  attrMat->removeAttributeArray(m_DataArrayName.c_str());
+	  }
   }
   typename DataArrayPixelType::Pointer data;
-  inputPtr->SetBufferedRegion(inputPtr->GetLargestPossibleRegion());
-  if(m_InPlace)
+  inputPtr->SetBufferedRegion( inputPtr->GetLargestPossibleRegion() );
+  if( m_InPlace )
   {
-    inputPtr->GetPixelContainer()->SetContainerManageMemory(false);
-    data = DataArrayPixelType::WrapPointer(reinterpret_cast<ValueType*>(inputPtr->GetBufferPointer()), attrMat->getNumberOfTuples(), cDims, this->GetDataArrayName().c_str(), true);
+    inputPtr->GetPixelContainer()->SetContainerManageMemory( false );
+    data = DataArrayPixelType::WrapPointer( reinterpret_cast<ValueType*>(inputPtr->GetBufferPointer()),
+              imageGeom->getNumberOfElements(), cDims, this->GetDataArrayName().c_str(), true );
   }
   else
   {
-    data = DataArrayPixelType::CreateArray(attrMat->getNumberOfTuples(), cDims, m_DataArrayName.c_str(), true);
-    if(nullptr != data.get())
+    data = DataArrayPixelType::CreateArray(imageGeom->getNumberOfElements(), cDims,
+              m_DataArrayName.c_str(), true);
+    if (nullptr != data.get())
     {
-      ::memcpy(data->getPointer(0), reinterpret_cast<ValueType*>(inputPtr->GetBufferPointer()), attrMat->getNumberOfTuples() * sizeof(ValueType));
+      ::memcpy(data->getPointer(0), reinterpret_cast<ValueType*>(inputPtr->GetBufferPointer()), imageGeom->getNumberOfElements() * sizeof(ValueType));
     }
   }
   attrMat->insertOrAssign(data);
@@ -161,33 +182,38 @@ void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::GenerateData()
 }
 
 // Check that names has been initialized correctly
-template <typename PixelType, unsigned int VDimension>
-void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::CheckValidArrayPathComponentName(std::string var) const
+template<typename PixelType, unsigned int VDimension>
+void
+InPlaceImageToDream3DDataFilter<PixelType, VDimension>
+::CheckValidArrayPathComponentName(std::string var) const
 {
-  if(var.find('/') != std::string::npos)
+  if (var.find('/') != std::string::npos)
   {
     itkExceptionMacro("Name contains a '/'");
   }
-  if(var.empty())
+  if (var.empty())
   {
     itkExceptionMacro("Name is empty");
   }
 }
 
+
 // Check that the inputs have been initialized correctly
-template <typename PixelType, unsigned int VDimension>
-void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::VerifyPreconditions() ITKv5_CONST
+template<typename PixelType, unsigned int VDimension>
+void
+InPlaceImageToDream3DDataFilter<PixelType, VDimension>
+::VerifyPreconditions() ITKv5_CONST
 {
-  // Test only works if image if of dimension 2 or 3
-  if(VDimension != 2 && VDimension != 3)
+  //Test only works if image if of dimension 2 or 3
+  if (VDimension != 2 && VDimension != 3)
   {
     itkExceptionMacro("Dimension must be 2 or 3.");
   }
   CheckValidArrayPathComponentName(m_AttributeMatrixArrayName);
   CheckValidArrayPathComponentName(m_DataArrayName);
   // Verify data container
-  const DecoratorType* outputPtr = this->GetOutput();
-  if(!outputPtr->Get())
+  const DecoratorType *outputPtr = this->GetOutput();
+  if (!outputPtr->Get())
   {
     itkExceptionMacro("Data container not set");
   }
@@ -196,18 +222,23 @@ void InPlaceImageToDream3DDataFilter<PixelType, VDimension>::VerifyPreconditions
 }
 
 /**
- *
- */
-template <typename PixelType, unsigned int VDimension>
-typename InPlaceImageToDream3DDataFilter<PixelType, VDimension>::DecoratorType* InPlaceImageToDream3DDataFilter<PixelType, VDimension>::GetOutput()
+*
+*/
+template<typename PixelType, unsigned int VDimension>
+typename InPlaceImageToDream3DDataFilter<PixelType, VDimension>::DecoratorType*
+InPlaceImageToDream3DDataFilter<PixelType, VDimension>
+::GetOutput()
 {
-  return itkDynamicCastInDebugMode<DecoratorType*>(this->GetPrimaryOutput());
+  return itkDynamicCastInDebugMode< DecoratorType * >(this->GetPrimaryOutput());
 }
 
-template <typename PixelType, unsigned int VDimension>
-const typename InPlaceImageToDream3DDataFilter<PixelType, VDimension>::DecoratorType* InPlaceImageToDream3DDataFilter<PixelType, VDimension>::GetOutput() const
+template<typename PixelType, unsigned int VDimension>
+const typename InPlaceImageToDream3DDataFilter<PixelType, VDimension>::DecoratorType*
+InPlaceImageToDream3DDataFilter<PixelType, VDimension>
+::GetOutput() const
 {
-  return itkDynamicCastInDebugMode<const DecoratorType*>(this->GetPrimaryOutput());
+  return itkDynamicCastInDebugMode< const DecoratorType * >(this->GetPrimaryOutput());
 }
 
-} // namespace itk
+} // end of itk namespace
+
