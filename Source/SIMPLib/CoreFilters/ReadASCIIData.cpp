d9f3bf295df7dac98139b093dfee3a86878e8e00
Michael Jackson <mike.jackson@bluequartz.net>
2019-05-17 13:17:05 -0400

    Do not check for an absolute path in the ReadASCII filter.
    The method being used makes assumptions about the layout of the binaries and where
    the executable is being run.
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Source/SIMPLib/CoreFilters/ReadASCIIData.cpp
+++ Source/SIMPLib/CoreFilters/ReadASCIIData.cpp
@@ -267,8 +267,6 @@ void ReadASCIIData::dataCheck()
     setErrorCondition(-387, ss);
   }
 
-  SIMPLDataPathValidator* validator = SIMPLDataPathValidator::Instance();
-  inputFilePath = validator->convertToAbsolutePath(inputFilePath);
   fi.setFile(inputFilePath);
 
   if(!fi.exists())
@@ -521,9 +519,6 @@ void ReadASCIIData::execute()
   }
 
   int insertIndex = 0;
-
-  SIMPLDataPathValidator* validator = SIMPLDataPathValidator::Instance();
-  inputFilePath = validator->convertToAbsolutePath(inputFilePath);
 
   QFile inputFile(inputFilePath);
   if(inputFile.open(QIODevice::ReadOnly))
