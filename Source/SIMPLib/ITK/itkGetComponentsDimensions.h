58f2f314ebfabfc64dc5a731a4e4d740527c985c
Megna Shah <megnashah@gmail.com>
2019-07-17 13:29:19 -0400

    Fix to cdims for ITK use (#371)

--- Source/SIMPLib/ITK/itkGetComponentsDimensions.h
+++ Source/SIMPLib/ITK/itkGetComponentsDimensions.h
@@ -16,19 +16,37 @@ template <class PixelType> QVector<size_t> GetComponentsDimensions_impl(PixelTyp
 
 template <class PixelType> QVector<size_t> GetComponentsDimensions_impl(itk::Vector<PixelType, 36>*)
 {
-  QVector<size_t> cDims(36, 1);
+  QVector<size_t> cDims(1, 36);
   return cDims;
 }
 
 template <class PixelType> QVector<size_t> GetComponentsDimensions_impl(itk::Vector<PixelType, 3>*)
 {
-  QVector<size_t> cDims(3, 1);
+  QVector<size_t> cDims(1, 3);
   return cDims;
 }
 
 template <class PixelType> QVector<size_t> GetComponentsDimensions_impl(itk::Vector<PixelType, 2>*)
 {
-  QVector<size_t> cDims(2, 1);
+  QVector<size_t> cDims(1, 2);
+  return cDims;
+}
+
+template <class PixelType> QVector<size_t> GetComponentsDimensions_impl(itk::Vector<PixelType, 4>*)
+{
+  QVector<size_t> cDims(1, 4);
+  return cDims;
+}
+
+template <class PixelType> QVector<size_t> GetComponentsDimensions_impl(itk::Vector<PixelType, 11>*)
+{
+  QVector<size_t> cDims(1, 11);
+  return cDims;
+}
+
+template <class PixelType> QVector<size_t> GetComponentsDimensions_impl(itk::Vector<PixelType, 10>*)
+{
+  QVector<size_t> cDims(1, 10);
   return cDims;
 }
 
