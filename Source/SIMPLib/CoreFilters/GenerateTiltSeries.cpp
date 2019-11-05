be92ca0f2050a1f02befe867c303e31f1c6a17ce
Jared Duffey <jared.duffey@bluequartz.net>
2019-11-05 14:31:00 -0500

    Added copyright notice
    * Ran clang-format

--- Source/SIMPLib/CoreFilters/GenerateTiltSeries.cpp
+++ Source/SIMPLib/CoreFilters/GenerateTiltSeries.cpp
@@ -1,6 +1,34 @@
-/*
- * Your License or Copyright can go here
- */
+/* ============================================================================
+ * Copyright (c) 2019 BlueQuartz Software, LLC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the names of any of the BlueQuartz Software contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *    United States Air Force Prime Contract FA8650-10-D-5210
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 #include "GenerateTiltSeries.h"
 
@@ -35,7 +63,6 @@
 #include "SIMPLib/ITK/itkInPlaceDream3DDataToImageFilter.h"
 #include "SIMPLib/Math/SIMPLibMath.h"
 #include "SIMPLib/SIMPLibVersion.h"
-
 
 #ifndef DREAM3D_PASSIVE_ROTATION
 #define DREAM3D_PASSIVE_ROTATION 1
@@ -893,7 +920,7 @@ GenerateTiltSeries::Pointer GenerateTiltSeries::NullPointer()
 // -----------------------------------------------------------------------------
 std::shared_ptr<GenerateTiltSeries> GenerateTiltSeries::New()
 {
-  struct make_shared_enabler : public GenerateTiltSeries  
+  struct make_shared_enabler : public GenerateTiltSeries
   {
   };
   std::shared_ptr<make_shared_enabler> val = std::make_shared<make_shared_enabler>();
@@ -972,5 +999,3 @@ QString GenerateTiltSeries::getOutputPrefix() const
 {
   return m_OutputPrefix;
 }
-
-
