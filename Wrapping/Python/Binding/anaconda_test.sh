3e1c3f13af7e2bb2cde4e6d1bb7e4b221db4ef82
Michael Jackson <mike.jackson@bluequartz.net>
2020-05-11 13:57:35 -0400

    Print out all the Python Testing environment variables for debugging
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Wrapping/Python/Binding/anaconda_test.sh
+++ Wrapping/Python/Binding/anaconda_test.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+echo "SIMPL_ANACONDA_DIR: $SIMPL_ANACONDA_DIR"
+echo "SIMPL_CONDA_ENV: $SIMPL_CONDA_ENV"
+echo "PYTHON_TEST_FILE: $PYTHON_TEST_FILE"
+echo "PYTHONPATH: $PYTHONPATH"
+
 source "$SIMPL_ANACONDA_DIR"/etc/profile.d/conda.sh
 
 conda activate "$SIMPL_CONDA_ENV"
+
+echo "PATH: $PATH"
+echo "Where is Python: " `which python`
+echo "Where is Conda: " `which conda`
+echo "Python Version: " `python --version`
 
 python "$PYTHON_TEST_FILE"
