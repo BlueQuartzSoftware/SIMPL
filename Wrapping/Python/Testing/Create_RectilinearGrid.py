455cd36055f0bc598ac82e7ac2c80d3782a21309
Jared Duffey <jared.duffey@bluequartz.net>
2020-07-17 15:18:25 -0400

    Fixed Create_RectilinearGrid.py
    * Data is now generated instead of read from a nonexistent file
    
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- Wrapping/Python/Testing/Create_RectilinearGrid.py
+++ Wrapping/Python/Testing/Create_RectilinearGrid.py
@@ -1,7 +1,4 @@
-
 # Based on CreateRectilinearGrid example
-
-# As of 9-13-18, this example will not work as the input file is not available
 
 import simpl
 import simplpy as d3d
@@ -16,30 +13,24 @@ def CreateRectilinearGridTest():
   err = d3d.create_data_container(dca, 'DataContainer')
   assert err == 0, f'DataContainer ErrorCondition: {err}'
 
-  # Import ASCII Data - Rectilinear Grid
-  importFile = sd.GetBuildDirectory() + '/Data/SIMPL/RectilinearGrid.csv'
-  wizardData = {
-    'inputFilePath': importFile,
-    'beginIndex': 2,
-    'numberOfLines': 15,
-    'delimiters': [','],    
-    'consecutiveDelimiters': False,
-    'automaticAM': True,
-    'selectedPath': simpl.DataArrayPath('DataContainer', 'Bounds', ''),
-    'headers': ['x', 'y', 'z'],
-    'attrMatType': 3,
-    'tupleDimensions': [14],
-    'dataTypes': ['float', 'float', 'float']
-  }
-  err = d3d.read_ascii_data(dca, wizardData)
-  assert err == 0, f'Import ASCII Data #1 -  ErrorCondition: {err}'
+  err = d3d.create_attribute_matrix(dca, simpl.DataArrayPath('DataContainer', 'Bounds', ''), simpl.AttributeMatrix.Type.Cell, sh.CreateDynamicTableData([[14]]))
+  assert err == 0, f'CreateAttributeMatrix ErrorCondition: {err}'
+
+  err = d3d.create_data_array(dca, simpl.ScalarTypes.Float, 1, simpl.DataArrayPath('DataContainer', 'Bounds', 'x'), 1, '0', (-50.0, 50.0))
+  assert err == 0, f'CreateDataArray ErrorCondition: {err}'
+
+  err = d3d.create_data_array(dca, simpl.ScalarTypes.Float, 1, simpl.DataArrayPath('DataContainer', 'Bounds', 'y'), 1, '0', (-50.0, 50.0))
+  assert err == 0, f'CreateDataArray ErrorCondition: {err}'
+
+  err = d3d.create_data_array(dca, simpl.ScalarTypes.Float, 1, simpl.DataArrayPath('DataContainer', 'Bounds', 'z'), 1, '0', (-50.0, 50.0))
+  assert err == 0, f'CreateDataArray ErrorCondition: {err}'
   
   # Create Geometry
-  err = sh.CreateGeometry(dca, 0, simpl.IGeometry.Type.Triangle, 'DataContainer', False, 
+  err = sh.CreateGeometry(dca, 0, simpl.IGeometry.Type.RectGrid, 'DataContainer', False, 
   x_bounds_array_path = simpl.DataArrayPath('DataContainer', 'Bounds', 'x'),
   y_bounds_array_path = simpl.DataArrayPath('DataContainer', 'Bounds', 'y'),
   z_bounds_array_path = simpl.DataArrayPath('DataContainer', 'Bounds', 'z'),
-  face_attribute_matrix_name = 'CellData')
+  cell_attribute_matrix_name = 'CellData')
   assert err == 0, f'Create Geometry -  ErrorCondition: {err}'
 
   err = d3d.data_container_writer(dca, sd.GetTestTempDirectory() + '/CreateRectilinearGrid.dream3d', True, False)
